18d39608265f08134b8d2508268f7e1903d12bfc
oussama236 <oussama.miladi@esprit.tn>
2022-11-17 05:34:13 +0100

    add gallerie form

--- src/Controller/Gallerie/GallerieController.php
+++ src/Controller/Gallerie/GallerieController.php
@@ -21,4 +21,25 @@ class GallerieController extends AbstractController
         $galleries = $repository->findAll();
         return $this->render("Gallerie/gallerie.html.twig", array("listGalleries" => $galleries));
     }
+
+    #[Route('/addGallerie', name: 'app_add_gallerie')]
+    public function addGallerie(Request $request, ManagerRegistry $doctrine, UserRepository $userRepository)
+    {
+        $gallerie = new Gallerie(); //création d'une instance gallerie pour l'ajouter dans la bdd.
+        $currentUser = $userRepository->find(1); //récupération de propriétaire d'id = 1. Statique pour le moment.
+
+        $gallerie->setEtat(0); // état = 0 : disponible pour la réserver, 1 sinon.
+        $gallerie->setUser($currentUser); //attribution de cla étrangère dans la table gallerie.
+
+        $form = $this->createForm(GallerieFormType::class, $gallerie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em = $doctrine->getManager();
+            $em->persist($gallerie);
+            $em->flush();
+            return $this->redirectToRoute("app_galleries");
+        }
+        return $this->renderForm("Gallerie/add_gallerie.html.twig", array("gallerieForm" => $form));
+    }
 }
